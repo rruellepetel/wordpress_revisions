f95fa6005d61ade2f0af599f08377fa41d44215e
rruellepetel <ruellepetelromain@gmail.com>
2017-08-29 13:57:51 +0200

    fix first section

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
                <span class="icon-bar"></span>
                <span class="icon-bar"></span>
                <span class="icon-bar"></span>
-               <div class="shake-horizontal">
+               <div>
                    <a class="navbar-brand" href="#">Flatfy</a>
                </div>
            </div>
@@ -39,17 +39,17 @@
                </ul>
            </div>
        </div>
-   </nav>
+  </nav>
 
-<div class="first-section text-center">
-   <div class="container" id="middle">
+<div class="first-section">
+   <div class="container">
    <h2>What is?</h2>
    <p class="stylish">A special thanks to Death.</p>
    </div>
    </div>
 
 <main>
-   <div class="container">
+   <div class="container container-main">
 
    <?php $args = array(  'orderby'  => 'date',
                          'order'    => 'ASC',
@@ -63,12 +63,11 @@
                   <p class="text-center"><?php the_content() ?></p>
                   </div>
                   <?php
-
               }?>
 
-           </div>
-<div class="tablet">
+    </div>
 
+<div class="tablet">
    <div class="container">
 
    <?php $args = array(  'orderby'  => 'date',
@@ -79,17 +78,17 @@
           foreach ($posts as $post) {
               setup_postdata($post); ?>
               <div class="col-md-6">
-                 <h2><?php the_title() ?></h2>
-                 <p class="text"><?php the_content() ?></p>
-                 <a class="btn btn-primary" href="#">View Details</a>
-                 <a class="btn btn-info" href="#">Visit Website</a>
+                <h2><?php the_title() ?></h2>
+                  <p class="text"><?php the_content() ?></p>
+                    <a class="btn btn-primary" href="#">View Details</a>
+                    <a class="btn btn-info" href="#">Visit Website</a>
               </div>
-               <div class='col-md-6'>
+                <div class='col-md-6'>
                   <?php the_post_thumbnail('full') ?>
-               </div>
+                </div>
               <?php
-
           }?>
+
    </div>
 </div>
 
@@ -106,13 +105,12 @@
                   <?php the_post_thumbnail('full') ?>
                </div>
                   <div class="col-md-6">
-                  <h2><?php the_title() ?></h2>
-                  <p class="text"><?php the_content() ?></p>
-                  <a class="btn btn-primary" href="#">View Details</a>
-                  <a class="btn btn-info" href="#">Visit Website</a>
-                  </div>
+                    <h2><?php the_title() ?></h2>
+                      <p class="text"><?php the_content() ?></p>
+                        <a class="btn btn-primary" href="#">View Details</a>
+                        <a class="btn btn-info" href="#">Visit Website</a>
+                   </div>
                       <?php
-
                   }?>
    </div>
 
@@ -135,14 +133,14 @@
                         <?php the_post_thumbnail('full') ?>
                      </div>
                               <?php
-
                           }?>
    </div>
 </div>
 
    <div class="container skate">
       <h2>Screen App</h2>
-      <p>Special Thanks To Death</p>
+        <p>Special Thanks To Death</p>
+
    <?php $args = array(  'orderby'  => 'date',
                          'category' => '9',
                          'order'    => 'ASC',
@@ -150,11 +148,10 @@
                         $posts = get_posts($args);
                         foreach ($posts as $post) {
                             setup_postdata($post); ?>
-                        <div class='col-md-4'>
+                            <div class='col-md-4'>
                         <?php the_post_thumbnail('full') ?>
-                        </div>
+                            </div>
                         <?php
-
                         }?>
    </div>
 
@@ -163,7 +160,7 @@
                 <div class="row">
                     <div class="col-md-6 col-md-offset-3 text-center white">
                         <h2>Get Live Updates</h2>
-                        <p class="stylish">A special thanks to Death.</p>
+                          <p class="stylish">A special thanks to Death.</p>
                     </div>
                     <div class="col-md-6 col-md-offset-3 text-center">
                         <div>
@@ -172,11 +169,11 @@
                     </div>
                 </div>
             </div>
-        </div>
+      </div>
 
         <div class="container credits">
            <h2>Crédits</h2>
-           <p>Special Thanks To Death</p>
+            <p>Special Thanks To Death</p>
         <?php $args = array(  'orderby'  => 'date',
                               'category' => '6',
                               'order'    => 'ASC',
@@ -189,7 +186,6 @@
                              <p class="text"><?php the_content() ?></p>
                              </div>
                              <?php
-
                              }?>
         </div>
 
@@ -204,57 +200,63 @@
                </div>
            </div>
        </div>
-   </div>
-                        <div class="background-section">
-         <div class="container">
-             <div class="row">
-                 <form role="form" action="" method="post">
-                     <div class="col-md-6">
-                         <div class="form-group">
-                             <label for="InputName">Your Name</label>
-                             <div class="input-group">
-                                 <input class="form-control" name="InputName" id="InputName" placeholder="Enter Name" required="" type="text">
-                                 <span class="input-group-addon"><i class="glyphicon glyphicon-ok form-control-feedback"></i></span>
-                             </div>
-                         </div>
-                         <div class="form-group">
-                             <label for="InputEmail">Your Email</label>
-                             <div class="input-group">
-                                 <input class="form-control" id="InputEmail" name="InputEmail" placeholder="Enter Email" required="" type="email">
-                                 <span class="input-group-addon"><i class="glyphicon glyphicon-ok form-control-feedback"></i></span>
-                             </div>
-                         </div>
-                         <div class="form-group">
-                             <label for="InputMessage">Message</label>
-                             <div class="input-group">
-                                 <textarea name="InputMessage" id="InputMessage" class="form-control" rows="5" required=""></textarea>
-                                 <span class="input-group-addon"><i class="glyphicon glyphicon-ok form-control-feedback"></i></span>
-                             </div>
-                         </div>
-                         <input style="visibility: visible;" name="submit" id="submit" value="Submit" class="btn btn-primary pull-right" type="submit">
-                     </div>
-                     <div class="col-md-5 col-md-push-1">
-                         <address>
-                  <h3>Office Location</h3>
-                  <p class="stylish">
+  </div>
+
+  <div class="background-section">
+    <div class="container">
+      <div class="row">
+        <form role="form" action="" method="post">
+          <div class="col-md-6">
+            <div class="form-group">
+              <label for="InputName">Your Name</label>
+                <div class="input-group">
+                  <input class="form-control" name="InputName" id="InputName" placeholder="Enter Name" required="" type="text">
+                   <span class="input-group-addon"><i class="glyphicon glyphicon-ok form-control-feedback"></i></span>
+                </div>
+            </div>
+
+            <div class="form-group">
+              <label for="InputEmail">Your Email</label>
+                <div class="input-group">
+                  <input class="form-control" id="InputEmail" name="InputEmail" placeholder="Enter Email" required="" type="email">
+                   <span class="input-group-addon"><i class="glyphicon glyphicon-ok form-control-feedback"></i></span>
+                </div>
+           </div>
+
+          <div class="form-group">
+            <label for="InputMessage">Message</label>
+              <div class="input-group">
+                <textarea name="InputMessage" id="InputMessage" class="form-control" rows="5" required=""></textarea>
+                  <span class="input-group-addon"><i class="glyphicon glyphicon-ok form-control-feedback"></i></span>
+              </div>
+          </div>
+          <input style="visibility: visible;" name="submit" id="submit" value="Submit" class="btn btn-primary pull-right" type="submit">
+          </div>
+
+          <div class="col-md-5 col-md-push-1">
+            <address>
+              <h3>Office Location</h3>
+                <p class="stylish">
                     The Pentagon<br>
                     Washington, DC 20301<br>
                     Phone: XXX-XXX-XXXX<br>
-                    Fax: XXX-XXX-YYYY</p>
-                </address>
-                         <h3>Social</h3>
-                         <li class="social">
-                             <a href="#"><i class="fa fa-facebook-square fa-size"> </i></a>
-                             <a href="#"><i class="fa  fa-twitter-square fa-size"> </i> </a>
-                             <a href="#"><i class="fa fa-google-plus-square fa-size"> </i></a>
-                             <a href="#"><i class="fa fa-flickr fa-size"> </i> </a>
-                         </li>
-                     </div>
-                 </form>
-             </div>
-         </div>
+                    Fax: XXX-XXX-YYYY
+                </p>
+            </address>
+              <h3>Social</h3>
+                <li class="social">
+                  <a href="#"><i class="fa fa-facebook-square fa-size"> </i></a>
+                  <a href="#"><i class="fa  fa-twitter-square fa-size"> </i> </a>
+                  <a href="#"><i class="fa fa-google-plus-square fa-size"> </i></a>
+                  <a href="#"><i class="fa fa-flickr fa-size"> </i> </a>
+                </li>
+            </div>
+        </form>
+    </div>
+      </div>
      </div>
 </main>
+
 <footer class="footer-grey">
       <div class="container">
           <div class="row">
